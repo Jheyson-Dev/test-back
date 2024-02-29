9f4de6a7160d34c8b5489ee9f4d9f71b9a2ac884
EtduardG <etduardgonzalo@gmail.com>
2024-02-29 12:28:08 -0500

    busqueda por codigo interno, oem, fabricante

---
+++ src/module/data/busquedaCodProductosRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/mysql');
+
+router.get('/', async (req, res) => {
+    try {
+        const query = req.query.busqueda;
+        if (query) {
+            const searchData = await db.buscarProductosPorCodigo(query);
+            return res.status(200).json(searchData);
+        } else {
+            return res.status(400).json({ error: true, message: 'Término de búsqueda no proporcionado' });
+        }
+    } catch (error) {
+        console.error('Error en la ruta /api/buscar-productos:', error);
+        return res.status(500).json({ error: true, message: 'Error interno' });
+    }
+});
+
+module.exports = router;
+
+module.exports = router;
