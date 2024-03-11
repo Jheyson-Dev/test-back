7d91d6f7b254a5a3b022e0c43acda88b1b9fdeba
EtduardG <etduardgonzalo@gmail.com>
2024-03-11 12:39:29 -0500

    update ingreso

--- src/module/ingreso/controller.js
+++ src/module/ingreso/controller.js
@@ -20,24 +20,21 @@ module.exports = function (dbInjected) {
             const id_producto = nuevoRegistro.id_producto;
             const id_tienda = nuevoRegistro.id_tienda;
             const cantidad = nuevoRegistro.cantidad;
-
+    
             // Verificar si la relación ya existe en tienda_producto
             const relacionExistente = await db.obtenerPorProductoYTienda(id_producto, id_tienda);
-
+    
             if (relacionExistente && relacionExistente.length > 0) {
-                // Si existe, actualizar el stock sumando la cantidad
                 const stockActual = relacionExistente[0].stock;
-                const nuevoStock = stockActual + cantidad;
+                const nuevoStock = parseInt(stockActual) + parseInt(cantidad);
                 await db.actualizarStockTiendaProducto(relacionExistente[0].id_tienda_producto, nuevoStock);
+                await db.agregar(tabla, nuevoRegistro); // Agregar el registro en la tabla de ingresos
+                return "Registro actualizado exitosamente";
             } else {
-                // Si no existe, crear una nueva relación con el stock igual a la cantidad
                 await db.crearRelacionTiendaProducto(id_producto, id_tienda, cantidad);
+                await db.agregar(tabla, nuevoRegistro); // Agregar el registro en la tabla de ingresos
+                return "Relacion creada y stock actualizado";
             }
-
-            // Agregar el registro a la tabla de ingreso
-            await db.agregar(tabla, nuevoRegistro);
-
-            return 'Registro agregado exitosamente';
         } catch (error) {
             throw error;
         }
